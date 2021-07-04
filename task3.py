5ec4eb6b293a0e15f4e14f41626102843d79815b
sidharth aggarwal <sidharth_aggarwal@mckinsey.com>
2021-07-04 10:25:35 +0530

    Exception handeling

--- task3.py
+++ task3.py
@@ -1,13 +1,15 @@
 import glom
 import json
-object_path = json.loads(input("Enter Object Path: "))
+import sys
+
+try:
+	object_path = json.loads(input("Enter Object Path: "))
+except json.decoder.JSONDecodeError:
+    print("Object path or format  is not correct")
+    sys.exit(1)
+
 key1 = input("Enter key value in format x/y/z: ")
-#object_path = {"x":{"y":{"z":"a"}}}
-#print(json.loads(object_path))
-#key1="x/y"
 key1 = key1.replace("/",".")
-#print(key1)
-actual = glom.glom(object_path, key1, default='path is not correct')
-print(actual)
 
-
+key_value = glom.glom(object_path, key1, default='path is not correct')
+print(key_value)
